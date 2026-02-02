6a3871d4434f8ef386dd07a9f00394ac6dcd0dad
tsukakoshi777 <owowo@oz0129.localdomain>
2026-02-02 13:48:24 +0900

    feat: ジェネレーター設定を追加

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module App
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework nil
+    end
   end
 end
