c40603e4e2454cf43a58d7cf3805aa8dae36e3cd
tsukakoshi777 <owowo@oz0129.localdomain>
2026-04-08 12:00:02 +0900

    Capybaraのポート設定を修正（ローカル: 3000, CI: 3001）してSeleniumとの競合を解消

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    if ENV['SELENIUM_REMOTE_URL']
-      # CI環境の場合
-      driven_by :selenium_chrome_headless
-    else
-      # ローカル環境の場合
-      driven_by :remote_chrome
-      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-      Capybara.server_port = 4444
-      Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-      Capybara.ignore_hidden_elements = false
-    end
-  end
+# CI環境用のドライバー設定
+Capybara.register_driver :selenium_remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: ENV.fetch('SELENIUM_REMOTE_URL', nil),
+    options: options
+  )
 end
 
 # ローカル環境用のドライバー設定
@@ -28,30 +29,30 @@ Capybara.register_driver :remote_chrome do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :remote,
-    url: 'http://chrome:4444/wd/hub',
+    url: ENV.fetch('SELENIUM_DRIVER_URL', 'http://chrome:4444/wd/hub'),
     options: options
   )
 end
 
-# CI環境用のドライバー設定
-Capybara.register_driver :selenium_chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--window-size=1400,1400')
+# RSpecの設定
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    if ENV['SELENIUM_REMOTE_URL']
+      # CI環境の設定
+      driven_by :selenium_remote_chrome
 
-  if ENV['SELENIUM_REMOTE_URL']
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: ENV['SELENIUM_REMOTE_URL'],
-      options: options
-    )
-  else
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+      Capybara.server_host = '0.0.0.0'
+      Capybara.server_port = 3001
+      Capybara.app_host = "http://#{`hostname -i`.strip}:#{Capybara.server_port}"
+    else
+      # ローカル環境の設定
+      driven_by :remote_chrome
+
+      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+      Capybara.server_port = 3000
+      Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    end
+
+    Capybara.ignore_hidden_elements = false
   end
 end
-
-Capybara.javascript_driver = :selenium_chrome_headless
