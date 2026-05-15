9cb6825d74343437eed0ab85478d542f78df5648
tsukakoshi777 <owowo@oz0129.localdomain>
2026-05-15 10:46:55 +0900

    feat: 詳細ページに星の成長度を示すGIFアニメーション追加

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -27,4 +27,22 @@ module GoalsHelper
     rate = (spark_count.to_f / total_required * 100).round
     [rate, 100].min # 100%を超えないようにする
   end
+
+  # 星のGIFアニメーションを返すメソッド
+  def star_gif(goal)
+    spark_count = goal.sparks.count
+
+    case spark_count
+    when 0..3
+      'star_stage_1.gif'
+    when 4..7
+      'star_stage_2.gif'
+    when 8..11
+      'star_stage_3.gif'
+    when 12..15
+      'star_stage_4.gif'
+    else # 16件以上
+      'star_stage_5.gif'
+    end
+  end
 end
--- app/models/spark.rb
+++ app/models/spark.rb
@@ -4,9 +4,6 @@ class Spark < ApplicationRecord
   belongs_to :user
   belongs_to :goal
 
-  # Spark（輝きメモ）とのアソシエーション
-  has_many :sparks, dependent: :destroy
-
   validates :content, presence: true, length: { maximum: 500 }
 
   # 新しい順に表示
